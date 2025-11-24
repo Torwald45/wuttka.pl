94ade8dc058b0722597a3f1607df18656e2caf74
Torwald45 <przemek@szkolenia.vip>
2025-11-24 18:00:01 +0100

    Fix site URL in astro.config, add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
-  site: 'https://41.pl',
-  integrations: [sitemap()],
+  site: 'https://wuttka.pl',
 });
